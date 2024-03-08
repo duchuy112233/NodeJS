7b12a326bd8c72ee9d38d07a1bc3536d98d08a05
Nguyễn Đức Huy - PH33852 <139211478+duchuy112233@users.noreply.github.com>
2024-03-08 18:01:52 +0700

    Update index.js

--- lesson1/index.js
+++ lesson1/index.js
@@ -3,26 +3,23 @@
 //      npm i nodemon
 //      node ./index.js
 
+const express = require("express");
+const app = express();
+const port = 3000;
 
-
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res,next) => {
-    console.log(`/ home`);
-    res.send(`trang home`)
-})
-app.get('/product', (req, res,next) => {
-    console.log(`product`);
-    res.send('trang products')
-})
-app.get('/product/:id', (req, res,next) => {
-    console.log(`product-detaill`);
-    res.send(`products detaill id: ${req.params.id} `)
-})
+app.get("/", (req, res, next) => {
+  console.log(`/ home`);
+  res.send(`trang home`);
+});
+app.get("/product", (req, res, next) => {
+  console.log(`product`);
+  res.send("trang products");
+});
+app.get("/product/:id", (req, res, next) => {
+  console.log(`product-detaill`);
+  res.send(`products detaill id: ${req.params.id} `);
+});
 
 app.listen(port, () => {
-    console.log(`Ung dung chay voi port ${port}`)
-})
-
+  console.log(`Ung dung chay voi port ${port}`);
+});
